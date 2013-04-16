c09fed8ee6bf8db8dee2fa78d9dd6d89cada26bf
Kyle <kg4332@desales.edu>
2013-04-15 22:11:54 -0400

    Added Pen/Shape Drawing capabilities

--- KG_App/KG_App/Form1.Designer.cs
+++ KG_App/KG_App/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tsBrush = new System.Windows.Forms.ToolStripLabel();
             this.tsSize = new System.Windows.Forms.ToolStripTextBox();
             this.Canvas = new System.Windows.Forms.Panel();
+            this.tsColorbtn = new System.Windows.Forms.ToolStripButton();
+            this.tsClearbtn = new System.Windows.Forms.ToolStripButton();
+            this.tsCanvasColorbtn = new System.Windows.Forms.ToolStripButton();
+            this.tsShapeBtn = new System.Windows.Forms.ToolStripSplitButton();
+            this.squareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsShapeSize = new System.Windows.Forms.ToolStripTextBox();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,10 +48,15 @@
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsBrush,
-            this.tsSize});
+            this.tsSize,
+            this.tsColorbtn,
+            this.tsCanvasColorbtn,
+            this.tsShapeBtn,
+            this.tsShapeSize,
+            this.tsClearbtn});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(404, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(835, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -56,24 +70,100 @@
             // 
             this.tsSize.Name = "tsSize";
             this.tsSize.Size = new System.Drawing.Size(100, 25);
+            this.tsSize.Text = "1";
             // 
             // Canvas
             // 
+            this.Canvas.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Canvas.Cursor = System.Windows.Forms.Cursors.Cross;
             this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Canvas.Location = new System.Drawing.Point(0, 25);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(404, 299);
+            this.Canvas.Size = new System.Drawing.Size(835, 426);
             this.Canvas.TabIndex = 1;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+            // 
+            // tsColorbtn
+            // 
+            this.tsColorbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsColorbtn.Image = ((System.Drawing.Image)(resources.GetObject("tsColorbtn.Image")));
+            this.tsColorbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsColorbtn.Name = "tsColorbtn";
+            this.tsColorbtn.Size = new System.Drawing.Size(66, 22);
+            this.tsColorbtn.Text = "Fore Color";
+            this.tsColorbtn.Click += new System.EventHandler(this.tsColorbtn_Click);
+            // 
+            // tsClearbtn
+            // 
+            this.tsClearbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsClearbtn.Image = ((System.Drawing.Image)(resources.GetObject("tsClearbtn.Image")));
+            this.tsClearbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsClearbtn.Name = "tsClearbtn";
+            this.tsClearbtn.Size = new System.Drawing.Size(79, 22);
+            this.tsClearbtn.Text = "Clear Canvas";
+            this.tsClearbtn.Click += new System.EventHandler(this.tsClearbtn_Click);
+            // 
+            // tsCanvasColorbtn
+            // 
+            this.tsCanvasColorbtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsCanvasColorbtn.Image = ((System.Drawing.Image)(resources.GetObject("tsCanvasColorbtn.Image")));
+            this.tsCanvasColorbtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsCanvasColorbtn.Name = "tsCanvasColorbtn";
+            this.tsCanvasColorbtn.Size = new System.Drawing.Size(68, 22);
+            this.tsCanvasColorbtn.Text = "Back Color";
+            this.tsCanvasColorbtn.Click += new System.EventHandler(this.tsCanvasColorbtn_Click);
+            // 
+            // tsShapeBtn
+            // 
+            this.tsShapeBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsShapeBtn.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.squareToolStripMenuItem,
+            this.rectToolStripMenuItem,
+            this.circleToolStripMenuItem});
+            this.tsShapeBtn.Image = ((System.Drawing.Image)(resources.GetObject("tsShapeBtn.Image")));
+            this.tsShapeBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsShapeBtn.Name = "tsShapeBtn";
+            this.tsShapeBtn.Size = new System.Drawing.Size(60, 22);
+            this.tsShapeBtn.Text = "Shapes";
+            // 
+            // squareToolStripMenuItem
+            // 
+            this.squareToolStripMenuItem.Name = "squareToolStripMenuItem";
+            this.squareToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.squareToolStripMenuItem.Text = "Square";
+            this.squareToolStripMenuItem.Click += new System.EventHandler(this.squareToolStripMenuItem_Click);
+            // 
+            // rectToolStripMenuItem
+            // 
+            this.rectToolStripMenuItem.Name = "rectToolStripMenuItem";
+            this.rectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rectToolStripMenuItem.Text = "Rect";
+            this.rectToolStripMenuItem.Click += new System.EventHandler(this.rectToolStripMenuItem_Click);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // tsShapeSize
+            // 
+            this.tsShapeSize.Name = "tsShapeSize";
+            this.tsShapeSize.Size = new System.Drawing.Size(40, 25);
+            this.tsShapeSize.Text = "50";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 324);
+            this.ClientSize = new System.Drawing.Size(835, 451);
             this.Controls.Add(this.Canvas);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Paint V1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -87,6 +177,14 @@
         private System.Windows.Forms.ToolStripLabel tsBrush;
         private System.Windows.Forms.ToolStripTextBox tsSize;
         private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.ToolStripButton tsColorbtn;
+        private System.Windows.Forms.ToolStripButton tsClearbtn;
+        private System.Windows.Forms.ToolStripButton tsCanvasColorbtn;
+        private System.Windows.Forms.ToolStripSplitButton tsShapeBtn;
+        private System.Windows.Forms.ToolStripMenuItem squareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox tsShapeSize;
     }
 }
 
--- KG_App/KG_App/Form1.cs
+++ KG_App/KG_App/Form1.cs
@@ -6,14 +6,129 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace KG_App
 {
     public partial class Form1 : Form
     {
+
+        //define necessary vars
+        bool canDraw = false;
+        Graphics g;
+        int? prevX, prevY  = null;
+        bool drawSquare,drawRect,drawCirc = false;
+
+        //init form
         public Form1()
         {
             InitializeComponent();
+            //for graphics
+            g = Canvas.CreateGraphics();
+
+        }
+
+        //MouseDown Even Handler
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            canDraw = true;
+            if (drawSquare)
+            {
+                SolidBrush s = new SolidBrush(tsColorbtn.ForeColor);
+                g.FillRectangle(s, e.X, e.Y, Convert.ToInt32(tsShapeSize.Text), Convert.ToInt32(tsShapeSize.Text));
+                canDraw = false;
+                drawSquare = false;
+            }
+            else if(drawRect)
+            {
+                SolidBrush s = new SolidBrush(tsColorbtn.ForeColor);
+                g.FillRectangle(s, e.X, e.Y, Convert.ToInt32(tsShapeSize.Text)*2, Convert.ToInt32(tsShapeSize.Text));
+                canDraw = false;
+                drawRect = false;
+            }
+            else if (drawCirc)
+            {
+                SolidBrush s = new SolidBrush(tsColorbtn.ForeColor);
+                g.FillEllipse(s, e.X, e.Y, Convert.ToInt32(tsShapeSize.Text), Convert.ToInt32(tsShapeSize.Text));
+                canDraw = false;
+                drawCirc = false;
+            }
+      
+        }
+
+        //Mouse Up Event Handler
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            canDraw = false;
+            prevX = null;
+            prevY = null;
+        }
+
+
+        //Mouse Movement event handler
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canDraw)
+            {
+                //do this
+                try
+                {
+                    Pen p = new Pen(tsColorbtn.ForeColor, float.Parse(tsSize.Text));
+                    g.DrawLine(p, new Point(prevX ?? e.X, prevY ?? e.Y), new Point(e.X, e.Y));
+                    prevX = e.X;
+                    prevY = e.Y;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //Button changes color we draw in
+        private void tsColorbtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                tsColorbtn.ForeColor = cd.Color;
+            }
+        }
+
+
+        //button clears canvas
+        private void tsClearbtn_Click(object sender, EventArgs e)
+        {
+            g.Clear(Canvas.BackColor);
+        }
+
+        //button changes canvas (background) color
+        private void tsCanvasColorbtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                tsCanvasColorbtn.ForeColor = cd.Color;
+                Canvas.BackColor = cd.Color;
+            }
+        }
+
+        //Square select
+        private void squareToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            drawSquare = true;
+        }
+
+        //Rectangle Select
+        private void rectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            drawRect = true;
+        }
+
+        //Circle Select
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            drawCirc = true;
         }
     }
 }
