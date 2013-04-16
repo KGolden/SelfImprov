94e2e01b0434af9269fb285ae6dc9477dbaa3a92
Kyle <kg4332@desales.edu>
2013-04-15 20:46:59 -0400

    fresh commit

---
+++ KG_App/KG_App/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace KG_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsBrush = new System.Windows.Forms.ToolStripLabel();
+            this.tsSize = new System.Windows.Forms.ToolStripTextBox();
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsBrush,
+            this.tsSize});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(404, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsBrush
+            // 
+            this.tsBrush.Name = "tsBrush";
+            this.tsBrush.Size = new System.Drawing.Size(83, 22);
+            this.tsBrush.Text = "Brush Size (px)";
+            // 
+            // tsSize
+            // 
+            this.tsSize.Name = "tsSize";
+            this.tsSize.Size = new System.Drawing.Size(100, 25);
+            // 
+            // Canvas
+            // 
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 25);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(404, 299);
+            this.Canvas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 324);
+            this.Controls.Add(this.Canvas);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel tsBrush;
+        private System.Windows.Forms.ToolStripTextBox tsSize;
+        private System.Windows.Forms.Panel Canvas;
+    }
+}
+
